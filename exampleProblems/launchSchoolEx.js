1ff0183bb4410818b2409a48ed420a0797986973
Rich H <richardharding305@gmail.com>
2022-12-15 22:51:41 -0600

    leet exampleProb Directory LS examples added

---
+++ exampleProblems/launchSchoolEx.js
@@ -0,0 +1,23 @@
+//  for
+
+for (let counter = 1; counter <= 10; counter += 1) {
+  console.log(counter);
+}
+
+//  while
+let count = 1;
+
+while (counter <= 10) {
+  console.log(counter);
+  counter += 1;
+}
+
+
+// do while
+
+let counter = 1;
+
+do {
+  console.log(counter);
+  counter += 1
+} while (counter <= 10);
